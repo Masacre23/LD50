33db33e634498684966a17264bf231ce58197444
Pablo <uriospablo@gmail.com>
2022-04-02 14:18:05 +0200

    cam change

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -31,8 +31,13 @@ public class PlayerController : MonoBehaviour
         movement = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
         if (movement != Vector3.zero)
         {
-            transform.rotation = Quaternion.Lerp(Quaternion.LookRotation(movement), transform.rotation, 0.45f);
+            //transform.rotation = Quaternion.Lerp(Quaternion.LookRotation(movement), transform.rotation, 0.45f);
 
+            
+            Quaternion q = transform.rotation;
+            q = Quaternion.Lerp(Quaternion.LookRotation(Camera.main.transform.TransformDirection(movement)), transform.rotation, 0.45f);                    
+            transform.rotation = Quaternion.Euler(0, q.eulerAngles.y, 0);
+            
             Vector3 camForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
             movement = Input.GetAxis("Vertical") * camForward + Input.GetAxis("Horizontal") * Camera.main.transform.right;
 
